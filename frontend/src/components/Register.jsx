6b5ffcde4f2f0b05c31e55ec40a75f1345cc2e06
Kenny Wong <z5424944@ad.unsw.edu.au>
2025-04-14 23:56:34 +1000

    Added state hook to register page

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import {
     BrowserRouter as Router,
     Routes,
@@ -5,8 +7,16 @@ import {
     Link,
 } from "react-router-dom"
 
+import axios from 'axios';
 
 function Register() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    console.log(email, password);
+
     return (
         <>
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -22,8 +32,9 @@ function Register() {
                         </label>
                         <div className="mt-2">
                             <input
-                            id="email"
-                            type="email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            type="text"
                             className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1
                             -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                             />
@@ -36,30 +47,49 @@ function Register() {
                             Password
                             </label>
 
+                        </div>
+                        <div className="mt-2">
+                            <input
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            type="password"
+                            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1
+                            -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                            />
+                        </div>
                     </div>
-                    <div className="mt-2">
-                        <input
-                        id="password"
-                        type="password"
-                        className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1
-                        -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                        />
+
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <label htmlFor="confirmPassword" className="block text-sm/6 font-medium text-gray-900">
+                            Confirm Password
+                            </label>
+
+                        </div>
+                        <div className="mt-2">
+                            <input
+                            value={confirmPassword}
+                            onChange={e => setConfirmPassword(e.target.value)}
+                            type="password"
+                            className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1
+                            -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                            />
+                        </div>
                     </div>
-                </div>
     
-                <div>
-                    <button
-                    className="mt-10 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6
-                    font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                        Register
-                    </button>
-                </div>
+                    <div>
+                        <button
+                        className="mt-10 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6
+                        font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Register
+                        </button>
+                    </div>
     
-                <p className="mt-10 text-center text-sm/6 text-gray-500">
-                    Already a member?{' '}
-                <Link to="/Login" className="font-semibold text-indigo-600 hover:text-indigo-500">Login Now!</Link>
-                </p>
+                    <p className="mt-10 text-center text-sm/6 text-gray-500">
+                        Already a member?{' '}
+                    <Link to="/Login" className="font-semibold text-indigo-600 hover:text-indigo-500">Login Now!</Link>
+                    </p>
                 </div>
             </div>
         </>
