16849f89f967689a3a4b9c9d6513b2f4c7fbeada
Kenny Wong <z5424944@ad.unsw.edu.au>
2025-04-25 15:16:36 +1000

    Fixed NavBar and MOdal linting

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,26 +1,30 @@
 function Modal({ open, onClose, children }) {
-    return (
-        <div onClick={onClose} className={`
-            fixed inset-0 flex justify-center items-center transition-colors backdrop-blur-sm overflow-y-auto
-            ${open ? "visible bg-black/20" : "invisible"}
+  return (
+    <div onClick={onClose} className={`
+      fixed inset-0 flex justify-center items-center transition-colors backdrop-blur-sm overflow-y-auto
+      ${open ? "visible bg-black/20" : "invisible"}
+    `}>
+
+      {/*modal*/}
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className={`
+        bg-white rounded-xl shadow p-6 transition-all max-h-[80vh] overflow-y-auto p-10
+        ${open ? "scale-100 opcity-100" : "scale-125 opacity-0"}
         `}>
-            {/*modal*/}
-            <div
-                onClick={(e) => e.stopPropagation()}
-                className={`
-                bg-white rounded-xl shadow p-6 transition-all max-h-[80vh] overflow-y-auto p-10
-                ${open ? "scale-100 opcity-100" : "scale-125 opacity-0"}
-                `}>
-                    <button onClick={onClose}
-                    className="absolute top-2  right-2  p-1 rounded-lg
-                    text-gray-400 bg-white hover:bg-gray-50
-                    hover:text-gray-600">
-                        Close
-                    </button>
-                    {children}
-            </div>
-        </div>
-    )
+
+        <button onClick={onClose}
+          className="absolute top-2  right-2  p-1 rounded-lg
+          text-gray-400 bg-white hover:bg-gray-50
+          hover:text-gray-600">
+            Close
+        </button>
+
+        {children}
+        
+      </div>
+    </div>
+  )
 }
 
 export default Modal
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,41 +2,41 @@ import { Outlet, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 function NavBar(props) {
-    const setToken = props.setToken;
-    const navigate = useNavigate();
+  const setToken = props.setToken;
+  const navigate = useNavigate();
 
-    const logout = async () => {
-        try {
-            const token = localStorage.getItem('token');
-            const res = await axios.post('http://localhost:5005/admin/auth/logout', {}, {
-                headers: { 'Authorization': `Bearer ${token}` }
-            });
-            localStorage.removeItem('token');
-            setToken(null);
-            navigate('/login');
-        } catch (err) {
-            alert(err.response.data.error);
-        }
-    };
-
-    const toDashboard = () => {
-        navigate('/dashboard');
+  const logout = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      await axios.post('http://localhost:5005/admin/auth/logout', {}, {
+        headers: { 'Authorization': `Bearer ${token}` }
+      });
+      localStorage.removeItem('token');
+      setToken(null);
+      navigate('/login');
+    } catch (err) {
+      alert(err.response.data.error);
     }
+  };
 
-    const toHistorySessions = () => {
-        navigate('/historySession');
-    }
+  const toDashboard = () => {
+    navigate('/dashboard');
+  }
 
-    return  (
+  const toHistorySessions = () => {
+    navigate('/historySession');
+  }
+
+  return  (
     <nav aria-label="Main navigation">
-        <div className='p-3 flex flex-row flex-nowrap justify-start gap-5 bg-gray-900 border-b rounded-b-lg'>
-            <button onClick={toDashboard} className='px-4 py-2 mr-2 text-white rounded hover:bg-gray-700 border'>Dashboard</button>
-            <button onClick={logout} className='px-4 py-2 text-white rounded hover:bg-gray-700 border'>Log Out</button>
-            <button onClick={toHistorySessions} className='px-4 py-2 text-white rounded hover:bg-gray-700 border'>History Sessions</button>
-        </div>
-        <Outlet />
+      <div className='p-3 flex flex-row flex-nowrap justify-start gap-5 bg-gray-900 border-b rounded-b-lg'>
+        <button onClick={toDashboard} className='px-4 py-2 mr-2 text-white rounded hover:bg-gray-700 border'>Dashboard</button>
+        <button onClick={logout} className='px-4 py-2 text-white rounded hover:bg-gray-700 border'>Log Out</button>
+        <button onClick={toHistorySessions} className='px-4 py-2 text-white rounded hover:bg-gray-700 border'>History Sessions</button>
+      </div>
+      <Outlet />
     </nav>
-    )
+  )
 }
 
 export default NavBar
